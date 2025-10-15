f0cd03bb44c954fad929dbb8eacdc2f560a3dac2
ntek-pengsoo <77426824+ntek-pengsoo@users.noreply.github.com>
2025-10-15 14:44:43 +0900

    Update DashboardCard_PQ_Claude2.vue

--- frontend/src/partials/inners/dashboard/DashboardCard_PQ_Claude2.vue
+++ frontend/src/partials/inners/dashboard/DashboardCard_PQ_Claude2.vue
@@ -6,7 +6,7 @@
         <h2 class="card-title">{{ t('dashboard.pq.title') }}</h2>
         <div class="channel-info">
           <span class="channel-text">
-            {{ channel == 'main' ? t('dashboard.meter.subtitle_main') : t('dashboard.meter.subtitle_sub') }}
+            {{ channel == 'Main' ? t('dashboard.meter.subtitle_main') : t('dashboard.meter.subtitle_sub') }}
           </span>
         </div>
       </header>
