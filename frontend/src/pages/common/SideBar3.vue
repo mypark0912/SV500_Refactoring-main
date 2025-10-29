cd90e536a6086b19d6998be27313c3041f7f2b31
ntek-pengsoo <77426824+ntek-pengsoo@users.noreply.github.com>
2025-10-29 14:03:35 +0900

    Update SideBar3.vue

--- frontend/src/pages/common/SideBar3.vue
+++ frontend/src/pages/common/SideBar3.vue
@@ -417,6 +417,13 @@
                 </a>
                 <div class="lg:hidden lg:sidebar-expanded:block 2xl:block">
                   <ul class="pl-8 mt-1" :class="!parentLink.expanded && 'hidden'">
+                    <router-link to="/config/Command" custom v-slot="{ href, navigate, isExactActive }">
+                      <li class="mb-1 last:mb-0">
+                        <a class="block transition truncate" :class="isExactActive ? 'text-violet-500' : 'text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200'" :href="href" @click="navigate">
+                          <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">System</span>
+                        </a>
+                      </li>
+                    </router-link>  
                     <router-link to="/config/Calibrate" custom v-slot="{ href, navigate, isExactActive }">
                       <li class="mb-1 last:mb-0">
                         <a class="block transition truncate" :class="isExactActive ? 'text-violet-500' : 'text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200'" :href="href" @click="navigate">
@@ -438,13 +445,7 @@
                         </a>
                       </li>
                     </router-link>    
-                    <router-link to="/config/Command" custom v-slot="{ href, navigate, isExactActive }">
-                      <li class="mb-1 last:mb-0">
-                        <a class="block transition truncate" :class="isExactActive ? 'text-violet-500' : 'text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200'" :href="href" @click="navigate">
-                          <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">System</span>
-                        </a>
-                      </li>
-                    </router-link>        
+      
                   </ul>
                 </div>
               </SidebarLinkGroup>                      
