9b212e518e969150d56fae49a9cd56d9666b9e3b
mypark0912 <oasis0912@gmail.com>
2026-06-15 15:15:07 +0900

    ZCT Type 변경 처리

--- webserver/frontend/src/partials/inners/setting/ChannelPanel.vue
+++ webserver/frontend/src/partials/inners/setting/ChannelPanel.vue
@@ -306,9 +306,8 @@
                       class="form-select w-full bg-white border-gray-300 rounded-md shadow-sm focus:ring-violet-500 focus:border-violet-500"
                     >
                       <option :value="0">None</option>
-                      <option :value="1">200mA:100mV</option>
-                      <option :value="2">200mA:1.5mA</option>
-                      <option :value="3">200mV:0.1mA</option>
+                      <option :value="1">200mA:1.5mA</option>
+                      <option :value="2">QZ3</option>
                     </select>
                   </div>
                 </div>
